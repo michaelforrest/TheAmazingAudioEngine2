0bb05f1996642dbc36135638c4a936270e0a2aba
Michael Forrest <michael.forrest@gmail.com>
2024-07-26 14:56:48 +0100

    Revert "Hack to make it not go weird when devices change"
    This reverts commit 7cc16f6d199fee8aa9e5507426faa2fc7dc2ef4d.

--- TheAmazingAudioEngine/Utilities/AEAudioDevice.m
+++ TheAmazingAudioEngine/Utilities/AEAudioDevice.m
@@ -71,8 +71,6 @@ NSString * const AEAudioDeviceAvailableDevicesChangedNotification = @"AEAudioDev
 }
 
 + (AEAudioDevice *)defaultOutputAudioDevice {
-    return nil;
-    // I never need this to be the default output and I wasted two hours trying to swizzle.
     AudioDeviceID deviceId;
     UInt32 size = sizeof(deviceId);
     AudioObjectPropertyAddress addr = {kAudioHardwarePropertyDefaultOutputDevice, kAudioObjectPropertyScopeGlobal};
